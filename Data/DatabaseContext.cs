efe2e20508cf56f2d2d24c7e9305c7446bc9937e
Israel Asidere <israelasidere@yahoo.com>
2022-01-13 14:56:33 +0100

    second commit

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -8,6 +8,57 @@ namespace HotelListing.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Nigeria",
+                    ShortName = "NG"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                }
+                );
+            builder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   Id = 1,
+                   Name = "Sandals Resorts and Spa",
+                   Address = "Negril",
+                   CountryId = 1,
+                   Rating = 4.5
+               },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Transcorp Resorts and Spa",
+                    Address = "Abuja",
+                    CountryId = 2,
+                    Rating = 4.5
+                },
+                 new Hotel
+                 {
+                     Id = 3,
+                     Name = "Grand Palldium",
+                     Address = "George Town",
+                     CountryId = 3,
+                     Rating = 4.5
+                 }
+
+
+               );
+        }
         public DbSet<Country> Countries { get; set; }
 
         public DbSet<Hotel> Hotels { get; set; }
---
+++ Migrations/20220113135207_SeedingData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Jamaica", "JM" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "Nigeria", "NG" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Bahamas", "BS" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1, "Negril", 1, "Sandals Resorts and Spa", 4.5 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2, "Abuja", 2, "Transcorp Resorts and Spa", 4.5 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 3, "George Town", 3, "Grand Palldium", 4.5 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
